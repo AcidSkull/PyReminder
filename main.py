1a5e4c66141a728fd9367c1f0ce95afce3b4983a
Patryk Jasik <zielonyfantom@interia.pl>
2020-12-07 20:11:13 +0100

    working on register

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, session, logging
+from flask.globals import request
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
@@ -19,6 +20,10 @@ class RegisterForm(Form):
 
 @app.route('/register', methods=['GET','POST'])
 def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        pass
+
     return render_template('register.html')
 
     

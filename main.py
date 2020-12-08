edece7aab4acd964c443c3c3c8460be4a8dbfaa2
Patryk Jasik <zielonyfantom@interia.pl>
2020-12-08 20:26:16 +0100

    added basic database config

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, session, logging
 from flask.globals import request
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators, BooleanField
+import sqlalchemy
+from wtforms import Form, StringField, PasswordField, validators, BooleanField
 from passlib.hash import sha256_crypt
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -17,13 +21,16 @@ class RegisterForm(Form):
         validators.EqualTo('confirm', message='Passwords do not match')
     ])
     confirm = PasswordField('Confirm password')
-    accepted_rules = BooleanField("I accpeted the terms of use",[validators.required()])
+    accepted_rules = BooleanField("I accpeted the terms of use",[validators.DataRequired()])
 
 @app.route('/register', methods=['GET','POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        username = form.data.username
+        username = form.username.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        return render_template('thanks.html')
 
     return render_template('register.html', form=form)
 

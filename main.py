4a945c3911389a56c8fc4b5df265e7641cae8f07
Patryk Jasik <zielonyfantom@interia.pl>
2020-12-09 21:19:03 +0100

    added uniqness in registration

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from enum import unique
 from flask import Flask, render_template, redirect, url_for, session, logging
 from flask.globals import request
+from flask.helpers import flash
+from flask_login.mixins import UserMixin
 from wtforms import Form, StringField, PasswordField, validators, BooleanField
 from passlib.hash import sha256_crypt
 from flask_sqlalchemy import SQLAlchemy
@@ -20,7 +23,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 @login_manager.user_loader
 def load_user(user_id):
-    pass
+    return Users.query.get(id)
 
 @app.route('/')
 def index():
@@ -28,9 +31,9 @@ def index():
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25), nullable=False)
+    username = db.Column(db.String(25), nullable=False, unique=True)
     password = db.Column(db.String(77), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
 
 class LoginForm(Form):
     username = StringField('Username', [validators.length(min=4, max=25)])
@@ -38,9 +41,22 @@ class LoginForm(Form):
         validators.DataRequired()
     ])
 
+class User(UserMixin,db.Model):
+    __tablename__ = 'Users'
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    email = db.Column(db.String)
+    password = db.Column(db.String)
+
+
+
 @app.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm()
+    # if form.validate_on_submit():
+    #     user = User.query.get(form.username.data)
+
+
 
     return render_template('login.html', form=form)
 
@@ -64,6 +80,13 @@ def register():
         password = sha256_crypt.encrypt(str(form.password.data))
 
         user = Users(username=username,password=password,email=email)
+
+        did_user_exist = Users.query.filter_by(email=email).first()
+        if did_user_exist:
+            flash('Email addres already exist!')
+            return render_template('register.html', form=form)
+
+
         db.session.add(user)
         db.session.commit()
 
